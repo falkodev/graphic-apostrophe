bbe5ea543be0ed1dc761394ba54985abfd9a70e4
Anthony Tarlao <anthony.tarlao@gmail.com>
2018-08-08 17:41:50 +0200

    fix config

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const apos = require('apostrophe')({
 
   modules: {
     // Apostrophe module configuration
-    'apostrophe-headless': {},
     // If a template is not found somewhere else, serve it from the top-level
     // `views/` folder of the project
     'apostrophe-templates': { viewsFolderFallback: path.join(__dirname, 'views') },
